1ee994fd93fdf7fe1f206506f771ec25e31eb6a8
WingYeeNg <wyng0@our.ecu.edu.au>
2021-10-01 21:17:05 +0800

    Add files via upload

--- task2.sh
+++ task2.sh
@@ -2,6 +2,6 @@
 
 read -p 'Enter the code you wish to search: ' selsrcip
 
-awk 'BEGIN {FS=","; NR>1} $4 ~ '$selsrcip' && $13 ~/suspicious/ { printf "%-5s %-15s %-10s %-15s %-10s %-5s %-10s %-10s\n", $3, $4, $5, $6, $7, $8, $9, $13}' < serv_acc_log_14032020.csv > tempresult.csv && cat tempresult.csv
+awk 'BEGIN {FS=","; NR>1} $4 ~ /'$selsrcip'/ && $13 ~/suspicious/ { printf "%-5s %-15s %-10s %-15s %-10s %-5s %-10s %-10s\n", $3, $4, $5, $6, $7, $8, $9, $13}' < serv_acc_log_12032020.csv > tempresult.csv && cat tempresult.csv
 
 exit 0
