f3049d8b9e356b127c241ebecd65ef94e02842ba
WingYeeNg <wyng0@our.ecu.edu.au>
2021-10-02 15:19:16 +0800

    Add files via upload

---
+++ task6.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sel=serv_acc_log_12032020.csv
+
+read -p "Enter a value you wish to search: " value
+read -p "Choose to search 1/ greater than, 2/ less than, 3/ equal to or 4/ not equal to $value [1,2,3 or 4]: " range
+
+grep "supspicious$" $sel > tempfile.csv
+
+case $range in
+     1) awk 'BEGIN {FS=","; ttlpackets=0; ttlbytes=0} 
+         NR>1 { 
+                if ( $8 > '"$value"' ) { ttlpackets=ttlpackets+$8
+                                        printf "%-6s %-15s %-10s %-15s %-10s %-5s %-10s %-10s\n", $3, $4, $5, $6, $7, $8, $9, $13 }
+               }
+                END { print "Total packets for all matching row is", ttlpackets }' < tempfile.csv > tempresult.csv && cat tempresult.csv;;
+     2) awk 'BEGIN {FS=","; ttlpackets=0; ttlbytes=0} 
+         NR>1 { 
+                if ( $8 < '"$value"' ) { ttlpackets=ttlpackets+$8
+                                                                printf "%-6s %-15s %-10s %-15s %-10s %-5s %-10s %-10s\n", $3, $4, $5, $6, $7, $8, $9, $13}
+                                                              }
+                                                              END { print "Total packets for all matching row is", ttlpackets }' < tempfile.csv > tempresult.csv && cat tempresult.csv;;
+     3) awk 'BEGIN {FS=","; NR>1} $8  ~/'"$value"'/ { ttlpackets=ttlpackets+$8
+                                                                printf "%-6s %-15s %-10s %-15s %-10s %-5s %-10s %-10s\n", $3, $4, $5, $6, $7, $8, $9, $13}
+                                                                END { print "Total packets for all matching row is", ttlpackets }' < tempfile.csv > tempresult.csv && cat tempresult.csv;;
+     4) awk 'BEGIN {FS=","; NR>1} $8  !~/'"$value"'/ { ttlpackets=ttlpackets+$8
+                                                                printf "%-6s %-15s %-10s %-15s %-10s %-5s %-10s %-10s\n", $3, $4, $5, $6, $7, $8, $9, $13}
+                                                                END { print "Total packets for all matching row is", ttlpackets }' < tempfile.csv > tempresult.csv && cat tempresult.csv;;
+     *) echo "Invalid input. Exit programme." && exit 1;;
+esac
+
+exit 0
