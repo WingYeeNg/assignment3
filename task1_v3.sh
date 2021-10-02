5caa8e5ce9a796b0c33347c3243c6b3a8a682e47
WingYeeNg <wyng0@our.ecu.edu.au>
2021-10-02 20:28:31 +0800

    Add files via upload

--- task1_v3.sh
+++ task1_v3.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-echo -e "1 TCP\n2 UDP\n3 ICMP\n4 GRE"
+echo -e "protocol:\n1 TCP\n2 UDP\n3 ICMP\n4 GRE"
 read -p 'Enter the number of key protocol above you wish to search [1,2,3 or 4]: ' selprotocol
 
 case $selprotocol in
-     1) awk 'BEGIN {FS=","} NR>1 ( $3 ~ /TCP/ && $13 ~ /suspicious/ ) { printf "%-3s %-15s %-10s %-15s %-10s %-5s %-10s\n", $3, $4, $5, $6, $7, $8, $9}' < serv_acc_log_12032020.csv > tempresult.csv && cat tempresult.csv;;
-     2) awk 'BEGIN {FS=","} NR>1 ( $3 ~ /UDP/ && $13 ~ /suspicious/ ) { printf "%-3s %-15s %-10s %-15s %-10s %-5s %-10s\n", $3, $4, $5, $6, $7, $8, $9}' < serv_acc_log_12032020.csv > tempresult.csv && cat tempresult.csv;;
-     3) awk 'BEGIN {FS=","} NR>1 ( $3~ /ICMP/ && $13 ~ /suspicious/ ) { printf "%-4s %-15s %-10s %-15s %-10s %-5s %-10s\n", $3, $4, $5, $6, $7, $8, $9}' < serv_acc_log_12032020.csv > tempresult.csv && cat tempresult.csv;;
-     4) awk 'BEGIN {FS=","} NR>1 ( $3~ /GRE/ && $13 ~ /suspicious/ ) { printf "%-3s %-15s %-10s %-15s %-10s %-5s %-10s\n", $3, $4, $5, $6, $7, $8, $9}' < serv_acc_log_12032020.csv > tempresult.csv && cat tempresult.csv;;
+     1) awk 'BEGIN {FS=","; NR>1} $3 ~/TCP/ && $13 ~/suspicious/ { printf "%-5s %-15s %-10s %-15s %-10s %-5s %-10s %-10s\n", $3, $4, $5, $6, $7, $8, $9, $13}' < serv_acc_log_12032020.csv > tempresult.csv && cat tempresult.csv;;
+     2) awk 'BEGIN {FS=","; NR>1} $3 ~/UDP/ && $13 ~/suspicious/ { printf "%-3s %-15s %-10s %-15s %-10s %-5s %-10s %-10s\n", $3, $4, $5, $6, $7, $8, $9, $13}' < serv_acc_log_12032020.csv > tempresult.csv && cat tempresult.csv;;
+     3) awk 'BEGIN {FS=","; NR>1} $3 ~/ICMP/ && $13 ~/suspicious/ { printf "%-4s %-15s %-10s %-15s %-10s %-5s %-10s %-10s\n", $3, $4, $5, $6, $7, $8, $9, $13}' < serv_acc_log_12032020.csv > tempresult.csv && cat tempresult.csv;;
+     4) awk 'BEGIN {FS=","; NR>1} $3 ~/GRE/ && $13 ~/suspicious/ { printf "%-3s %-15s %-10s %-15s %-10s %-5s %-10s %-10s\n", $3, $4, $5, $6, $7, $8, $9, $13}' < serv_acc_log_12032020.csv > tempresult.csv && cat tempresult.csv;;
      *) echo "Invalid selection. Exit Program." && exit 1;;
 esac
+
+echo -e "\n"
+read -p 'Name your output file: ' filename
+mv tempresult.csv $filename.csv && echo "The $filename.csv outputfile is saved." && ls
 
 exit 0
--- task7.sh
+++ task7.sh
@@ -4,50 +4,49 @@ read -p "Enter a value you wish to search: " value
 read -p "Choose to search 1/ greater than, 2/ less than, 3/ equal to or 4/ not equal to $value [1,2,3 or 4]: " range
 
 sel=serv_acc_log_14032020.csv
-cct=$value
 
 grep "suspicious$" $sel > tempfile.csv
 
 case $range in
      1) awk ' BEGIN {FS=","; ttlpackets=0; ttlbytes=0} 
      NR>1 {
-           if ( $8 > '"$value"' )
+           if ( $9 > '"$value"' )
             { 
-              ttlpackets=ttlpackets+$8
+              ttlpackets=ttlpackets+$9
               printf "%-6s %-15s %-10s %-15s %-10s %-5s %-10s \n", $3, $4, $5, $6, $7, $8, $9
             } 
            }
-           END { print "Total packets for all matching rows is ", ttlpackets }
+           END { print "Total size for all matching rows is ", ttlpackets, " bytes." }
            ' < tempfile.csv > tempresult.csv && cat tempresult.csv;; 
      2) awk ' BEGIN {FS=","; ttlpackets=0; ttlbytes=0} 
      NR>1 {
-           if ( $8 < '"$value"' )
+           if ( $9 < '"$value"' )
             { 
-              ttlpackets=ttlpackets+$8
+              ttlpackets=ttlpackets+$9
               printf "%-6s %-15s %-10s %-15s %-10s %-5s %-10s \n", $3, $4, $5, $6, $7, $8, $9
             } 
            }
-           END { print "Total packets for all matching rows is ", ttlpackets }
+           END { print "Total size for all matching rows is ", ttlpackets, " bytes." }
            ' < tempfile.csv > tempresult.csv && cat tempresult.csv;;
      3) awk ' BEGIN {FS=","; ttlpackets=0; ttlbytes=0} 
      NR>1 {
-           if ( $8 = '"$value"' )
+           if ( $9 = '"$value"' )
             { 
-              ttlpackets=ttlpackets+$8
+              ttlpackets=ttlpackets+$9
               printf "%-6s %-15s %-10s %-15s %-10s %-5s %-10s \n", $3, $4, $5, $6, $7, $8, $9
             } 
            }
-           END { print "Total packets for all matching rows is ", ttlpackets }
+           END { print "Total size for all matching rows is ", ttlpackets, " bytes." }
            ' < tempfile.csv > tempresult.csv && cat tempresult.csv;;
      4) awk ' BEGIN {FS=","; ttlpackets=0; ttlbytes=0} 
      NR>1 {
-           if ( $8 != '"$value"' )
+           if ( $9 != '"$value"' )
             { 
-              ttlpackets=ttlpackets+$8
+              ttlpackets=ttlpackets+$9
               printf "%-6s %-15s %-10s %-15s %-10s %-5s %-10s \n", $3, $4, $5, $6, $7, $8, $9
             } 
            }
-           END { print "Total packets for all matching rows is ", ttlpackets }
+           END { print "Total size for all matching rows is ", ttlpackets, " bytes." }
            ' < tempfile.csv > tempresult.csv && cat tempresult.csv;;
      *) echo "Invalid input. Exit programme." && exit 1;;
 esac
